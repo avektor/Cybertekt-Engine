09f4c15b766480a651f22c7790a4a841c418e134
Vektor <Vektor@192.168.2.6>
2017-10-14 11:49:32 -0400

    Reverted previous changes to AssetManager.

--- src/net/cybertekt/asset/AssetManager.java
+++ src/net/cybertekt/asset/AssetManager.java
@@ -36,10 +36,10 @@ import org.slf4j.LoggerFactory;
  *
  * <p>
  * The static methods provided by this class are <b>NOT</b> thread-safe! This
- * was a conscious design choice as the asset manager itself spawns new threads
- * as needed to handle the requested asset loading tasks. The static methods
- * provided by this class should <b>ONLY</b> be called from the main application
- * thread.
+ * was a conscious design choice as the asset manager itself spawns new daemon
+ * threads as needed to handle the requested asset loading tasks. The static
+ * methods provided by this class should <b>ONLY</b> be called from the main
+ * application thread.
  * </p>
  *
  * <p>
@@ -791,8 +791,8 @@ public final class AssetManager {
     }
 
     /**
-     * Factory class for generating the threads used for the concurrent loading
-     * of {@link Asset assets}.
+     * Factory class for generating the daemon threads used for the concurrent
+     * loading of {@link Asset assets}.
      */
     private static final class AssetThreadFactory implements ThreadFactory {
 
@@ -807,15 +807,15 @@ public final class AssetManager {
         private final String prefix = "Assets-";
 
         /**
-         * Constructs and returns a asset thread.
+         * Constructs and returns a new daemon asset thread.
          *
          * @param task the runnable task for which to create the thread.
-         * @return the constructed thread.
+         * @return the constructed daemon thread.
          */
         @Override
         public final Thread newThread(final Runnable task) {
             Thread t = new Thread(task, prefix + count++);
-            //t.setDaemon(true);
+            t.setDaemon(true);
             return t;
         }
     }
