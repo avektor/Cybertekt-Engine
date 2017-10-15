380ddf7c6d1812821f66a4ab269abaf3cac99c47
Vektor <Vektor@192.168.2.6>
2017-10-14 20:29:23 -0400

    Added rendering to shader test class.

--- test/net/cybertekt/asset/ShaderTest.java
+++ test/net/cybertekt/asset/ShaderTest.java
@@ -1,6 +1,5 @@
  package net.cybertekt.asset;
 
-import java.nio.FloatBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import net.cybertekt.app.BasicApplication;
@@ -9,29 +8,22 @@ import net.cybertekt.display.Display;
 import net.cybertekt.display.input.Input;
 import net.cybertekt.display.input.InputMapping;
 import net.cybertekt.exception.OGLException;
+import net.cybertekt.mesh.Mesh;
 import net.cybertekt.render.OGLShaderProgram;
 import net.cybertekt.render.Renderer;
 import static org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray;
-import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glDrawArrays;
+import static org.lwjgl.opengl.GL11.glDrawElements;
 import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
-import static org.lwjgl.opengl.GL15.glBindBuffer;
-import static org.lwjgl.opengl.GL15.glBufferData;
-import static org.lwjgl.opengl.GL15.glGenBuffers;
 import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
 import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
 import static org.lwjgl.opengl.GL20.glUseProgram;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
-import org.lwjgl.system.MemoryUtil;
 
 /**
  * Shading System Unit Test - (C) Cybertekt Software
@@ -50,15 +42,25 @@ public class ShaderTest extends BasicApplication implements Renderer {
      */
     private final Map<String, OGLShaderProgram> shaders = new HashMap<>();
     
-    private int vertexArrayId;
-    
-    private int vertexBufferId;
-    
     private final float[] vertices = new float[] {
-        0.0f, 0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+        0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f
     };
+    
+    private final float[] colors = new float[] {
+        0.5f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.0f, 0.0f, 0.5f,
+        0.0f, 0.5f, 0.5f
+    };
+    
+    private final int[] indices = new int[] {
+        0, 1, 3, 3, 1, 2
+    };
+    
+    private Mesh quad;
 
     public static void main(final String[] args) {
         ShaderTest app = new ShaderTest();
@@ -75,7 +77,6 @@ public class ShaderTest extends BasicApplication implements Renderer {
         display.addInputMapping("print", new InputMapping(Input.Key.P).when(Input.State.Pressed));
         
         long time = System.nanoTime();
-        
         
         try {
             /* Load OpenGL Shader Programs */
@@ -97,25 +98,7 @@ public class ShaderTest extends BasicApplication implements Renderer {
         
         /* Log the number and total time required to initialize the shader programs */
         log.info("{} shader(s) loaded in {}ms", shaders.size(), (after - time) / 1000000);
-        
-        
-        /* Generate Vertex Array and Vertex Buffers */
-        FloatBuffer vertexBuffer = MemoryUtil.memAllocFloat(vertices.length);
-        vertexBuffer.put(vertices).flip();
-        
-        vertexArrayId = glGenVertexArrays();
-        glBindVertexArray(vertexArrayId);
-
-        vertexBufferId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
-        
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(vertexArrayId);
-        
-        MemoryUtil.memFree(vertexBuffer);
+        quad = new Mesh(vertices, colors, indices);
     }
     
     @Override
@@ -128,30 +111,40 @@ public class ShaderTest extends BasicApplication implements Renderer {
      */
     @Override
     public final void render() {
-        glClearColor(0.5f, 0f, 0.5f, 0f);
+        
+        /* Clear The Display */
+        glClearColor(0.0f, 0f, 0.0f, 1f);
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        /* Bind the Shader Program to use for rendering. */
+        /* Bind the Shader */
         glUseProgram(shaders.get("Solid").getId());
         
-        /* Bind and enable the vertex array object. */
-        glBindVertexArray(vertexArrayId);
-        glEnableVertexAttribArray(0);
+        /* Bind The Vertex Attribute Array */
+        glBindVertexArray(quad.getVertexArrayId());
         
-        /* Draw the vertices */
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        /* Enable Vertex Position and Color Attribute Arrays */
+        glEnableVertexAttribArray(0); //Vertex Position Attribute In Solid Shader.
+        glEnableVertexAttribArray(1); //Vertex Color Attribute In Solid Shader.
         
-        /* Restore the default state and unbind the shader program */
-        glDisableVertexAttribArray(0);
+        /* Render The Quad */
+        glDrawElements(GL_TRIANGLES, quad.getVertexCount(), GL_UNSIGNED_INT, 0);
+        
+        /* Disable Vertex Position and Color Attribute Arrays */
+        glDisableVertexAttribArray(0); //Vertex Position Attribute In Solid Shader.
+        glDisableVertexAttribArray(1); //Vertex Color Attribute In Solid Shader.
+        
+        /* Unbind The Vertex Attribute Array */
         glBindVertexArray(0);
         
+        /* Unbind The Shader Program */
         glUseProgram(0);
     }
     
     @Override
     public final void stop() {
         super.stop();
+        quad.destroy();
     }
     
     @Override
