9ee417309e8c5aeae38453b9f18904ff566938b8
Vektor <Vektor@192.168.2.6>
2017-10-17 15:08:03 -0400

    Added shader loading to the asset test class.

--- test/net/cybertekt/asset/AssetTest.java
+++ test/net/cybertekt/asset/AssetTest.java
@@ -1,7 +1,8 @@
- package net.cybertekt.asset;
+package net.cybertekt.asset;
 
 import java.util.Random;
 import net.cybertekt.asset.image.ImageLoader;
+import net.cybertekt.asset.shader.ShaderLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,8 +23,8 @@ public class AssetTest {
     }
 
     public void init() {
-        //AssetManager.registerLoader(new ImageLoader(), AssetType.getType("PNG"));
         AssetManager.registerLoader(ImageLoader.class, AssetType.getType("PNG"));
+        AssetManager.registerLoader(ShaderLoader.class, AssetType.getType("FRAG"), AssetType.getType("VERT"));
 
         //long nanoTime = System.nanoTime();
         //Image pngFallback = AssetManager.get("Textures/PNG/Grayscale.png", Image.class);
@@ -44,11 +45,12 @@ public class AssetTest {
     public void start() {
         log.info("Application Started - Asset Count is {} - Total Available Processors is {}", AssetManager.getLoaded(), Runtime.getRuntime().availableProcessors());
         long time = System.nanoTime();
-        
+
         // Remove the fallback asset and a runtime exception will occur if you attempt to retrieve a non-existent or invalid PNG asset such as "Textures/PNG/Bad.png" or "Textures/PNG/DoesNotExist.png".
         AssetManager.setFallback(AssetManager.get("Textures/PNG/Grayscale.png"), AssetType.getType("PNG"));
         
-        AssetManager.load("Textures/PNG/Grayscale.png");
+        //Load Images
+        AssetManager.load("Textures/PNG/Grayscale.png"); //Already loaded as a fallback asset. Asset Manager should prevent the same asset from being loaded more than once.
         AssetManager.load("Textures/PNG/IDX8.png");
         AssetManager.load("Textures/PNG/LUM8.png");
         AssetManager.load("Textures/PNG/LUMA8.png");
@@ -57,14 +59,15 @@ public class AssetTest {
         AssetManager.load("Textures/PNG/RGBA08.png");
         AssetManager.load("Textures/PNG/RGBA082.png");
         AssetManager.load("Textures/PNG/RGBA16.png");
-        AssetManager.load("Textures/PNG/Bad.png");
-        AssetManager.load("Textures/PNG/DoesNotExist.png");
-        AssetManager.load("Textures/PNG/DoesNotExist2.png");
+        
+        //Load Shaders
+        AssetManager.load("Shaders/solid.vert");
+        AssetManager.load("Shaders/solid.frag");
         
         //No Exception Thrown Here If A Fallback Asset Has Been Registered For PNG Asset Types.
-        AssetManager.get("Textures/PNG/Bad.png");
-        //AssetManager.get("Textures/PNG/DoesNotExist.png");
-        //AssetManager.get("Textures/PNG/DoesNotExist2.png");
+        AssetManager.load("Textures/PNG/Bad.png"); //Not A Valid PNG Image File.
+        AssetManager.load("Textures/PNG/DoesNotExist.png"); //Image File Does Not Exist.
+        AssetManager.load("Textures/PNG/DoesNotExist2.png"); //Image File Does Not Exist.
         
         long loops = 0;
         log.info("Now Loading ... {}/{}/{} - {}%", AssetManager.getLoaded(), AssetManager.getFailed(), AssetManager.getRequested(), String.format("%.0f", AssetManager.getProgress() * 100f));
@@ -74,7 +77,7 @@ public class AssetTest {
         }
         log.info("Loading Done - {}/{}/{} - {}% ({} Threads - {} Loops)", AssetManager.getLoaded(), AssetManager.getFailed(), AssetManager.getRequested(), String.format("%.0f", AssetManager.getProgress() * 100f), AssetManager.getPoolSize(), loops);
         log.info("Loaded {} of {} assets in {}ms using {} thread(s) - {} Cached - (Active Threads: {})", AssetManager.getLoaded(), AssetManager.getRequested(), Math.round((float) (System.nanoTime() - time) / 1000000L), AssetManager.getPoolSize(), AssetManager.getCacheSize(), Thread.activeCount());
-        
+
         //AssetManager.cachedAssets.keySet().forEach((a) -> {
         //    log.info("Cached Asset Key - {}", a.getName());
         //});
